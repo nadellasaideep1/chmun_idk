d5e157fcbde3d35d6bba4abde99edfa5f5da1bcd
Archit Kumar <86660022+Archit1712Coder@users.noreply.github.com>
2025-07-29 10:21:44 +0000

    letters

--- src/App.jsx
+++ src/App.jsx
@@ -218,10 +218,10 @@ function App() {
                 <motion.p className="text-2xl font-serif">
                   {closing.split("\n")[1]}
                 </motion.p>
-                                <motion.p className="text-xl font-serif">
+                                <motion.p className="text-xl pt-2  font-serif">
                   {closing.split("\n")[2]}
                 </motion.p>
-                                <motion.p className="text-xl font-serif">
+                                <motion.p className="text-xl py-1 font-serif">
                   {closing.split("\n")[3]}
                 </motion.p>
               </motion.div>
